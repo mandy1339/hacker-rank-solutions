b232f2e24c9325d8d114286626f3c1012eac197e
mandy1339 <mandy1339@gmail.com>
2018-09-17 22:47:08 -0400

    added javascript tutorial solutions for day 2 and 3

---
+++ 10_days_of_javascript/05-day-01-let-and-const.js
@@ -0,0 +1,10 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const rad = Number(readLine());
+    const PI = Math.PI;
+    
+    // Print the area of the circle:
+    console.log(PI * rad ** 2);
+    
+    // Print the perimeter of the circle:
+    console.log(2*PI * rad)
